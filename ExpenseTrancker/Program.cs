a590c6cfe504a34f1b408678c2d54e3f8ed05f0b
Bishal Poudel <bishal.poudel@crystalsolution.com.np>
2024-03-09 15:33:28 +0545

    intital migration and syncfusion package added

--- ExpenseTrancker/Program.cs
+++ ExpenseTrancker/Program.cs
@@ -1,7 +1,14 @@
+using ExpenseTrancker.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
 
 var app = builder.Build();
 
--- ExpenseTrancker/obj/Debug/net8.0/ExpenseTrancker.AssemblyInfo.cs
+++ ExpenseTrancker/obj/Debug/net8.0/ExpenseTrancker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseTrancker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24ab3de6ebbdf45c48f03595c4a5a4772d848871")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseTrancker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseTrancker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
