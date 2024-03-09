24ab3de6ebbdf45c48f03595c4a5a4772d848871
Bishal Poudel <bishal.poudel@crystalsolution.com.np>
2024-03-09 08:24:58 +0545

    Initial Commit

---
+++ ExpenseTrancker/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ExpenseTrancker.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
